816eb08d7ce77a7525f96dbeae626dfef0580cfb
gcullinan <glc21@duke.edu>
2019-07-19 01:44:48 -0600

    fisheries maping start

--- statia_fisheries_data_scoping.R
+++ statia_fisheries_data_scoping.R
@@ -162,12 +162,12 @@ anti.join.conch <- log.data %>% # use this join to see what values are not joini
 ## subset of observations 
 
 ##################################### beginning to filter by species per year and month ###########################
-
+####### do it by gear instead of by species and by date 
 unique(joined.fish$Species_latin_name)
 
 distinct.species.year <- joined.fish %>% # looking at the number of distinct species per year
-  group_by(Year) %>%
-  select(Year, Species_latin_name) %>%
+  group_by(Gear.x) %>%
+  #select(Gear.x, Species_latin_name) %>%
   summarize(n_distinct(Species_latin_name))
 
 species.year <- joined.fish %>% # looking at number of individuals per species per year 
@@ -193,9 +193,11 @@ distinct.species.year.gear <- joined.fish %>% # looking at the number of distinc
   summarize(n_distinct(Species_latin_name))
 
 species.year.gear <- joined.fish %>% # looking at number of individuals per species per year per gear
-  group_by(Year, Gear.x) %>%
-  select(Year, Gear.x, Species_latin_name) %>%
+  group_by( Gear.x, Species_latin_name) %>%
+  #select(Year, Gear.x, Species_latin_name) %>%
   count(Species_latin_name, name = "Count")
+#have the weights. so could do weights by family
+
 
 distinct.species.month.gear <- joined.fish %>% # looking at the number of distinct species per month per gear
   group_by(Year, Month, Gear.x) %>%
@@ -221,22 +223,19 @@ head(zones.fish)
 
 #amount of fish per zone per year
 zone.fish.year <- zones.fish %>% 
-  group_by(Year,Landings,zone_id)%>% 
-  filter(Landings=="Fish") %>%
-  summarize(weight.total=sum(weight.per.zone,na.rm = T))
+ group_by(Year,Landings,zone_id)%>% 
+ summarize(weight.total=sum(weight.per.zone,na.rm = T))
 View(zone.fish.year)
 
 #amount of fish per zone per month
 zone.fish.month <- zones.fish %>% 
   group_by(Year,Month,Landings,zone_id)%>% 
-  filter(Landings=="Fish") %>%
   summarize(weight.total=sum(weight.per.zone,na.rm = T))
 View(zone.fish.month)
 
 #amount of fish per zone per year per gear
 zone.fish.gear.year<-zones.fish %>%
   group_by(Year, Landings, Gear, zone_id) %>%
-  filter(Landings=="Fish") %>%
   select(Year, Landings, Gear, zone_id, weight.per.zone)%>%
   summarize(weight.total=sum(weight.per.zone,na.rm = T))
 View(zone.fish.gear.year)  
@@ -244,7 +243,6 @@ View(zone.fish.gear.year)
 #amount of fish per zone per month per gear
 zone.fish.gear.month<-zones.fish %>%
   group_by(Year, Month, Landings, Gear, zone_id) %>%
-  filter(Landings=="Fish") %>%
   select(Year, Month, Landings, Gear, zone_id, weight.per.zone)%>%
   summarize(weight.total=sum(weight.per.zone,na.rm = T))
 View(zone.fish.gear.month)  
@@ -252,7 +250,6 @@ View(zone.fish.gear.month)
 #number of trips for fish per zone per year
 zone.fish.trips.year <- zones.fish %>% 
   group_by(Year,Landings,zone_id)%>% 
-  filter(Landings=="Fish") %>%
   select(Year, Landings, Trip_ID,zone_id) %>%
   summarize(Num.Trips=n_distinct(Trip_ID))
 View(zone.fish.trips.year)
@@ -260,7 +257,6 @@ View(zone.fish.trips.year)
 #number of trips for fish per zone per month
 zone.fish.trips.monthly <- zones.fish %>% 
   group_by(Year,Month,Landings,zone_id)%>% 
-  filter(Landings=="Fish") %>%
   select(Year, Month, Landings, Trip_ID,zone_id) %>%
   summarize(Num.Trips=n_distinct(Trip_ID))
 View(zone.fish.trips.monthly)
@@ -268,7 +264,6 @@ View(zone.fish.trips.monthly)
 #number of trips for fish per zone per year per gear
 zone.fish.trips.gear.year <- zones.fish %>% 
   group_by(Year,Landings,zone_id, Gear)%>% 
-  filter(Landings=="Fish") %>%
   select(Year, Landings, Trip_ID,zone_id, Gear) %>%
   summarize(Num.Trips=n_distinct(Trip_ID))
 View(zone.fish.trips.gear.year)
@@ -276,49 +271,43 @@ View(zone.fish.trips.gear.year)
 #number of trips for fish per zone per month per gear
 zone.fish.trips.gear.monthly <- zones.fish %>% 
   group_by(Year,Month, Landings,zone_id, Gear)%>% 
-  filter(Landings=="Fish") %>%
   select(Year, Month, Landings, Trip_ID,zone_id, Gear) %>%
   summarize(Num.Trips=n_distinct(Trip_ID))
 View(zone.fish.trips.gear.monthly)
 
 #amount of fish caught per zone for each unique trip ID for each year
-zone.fish.uni.trips.year <- zones.fish%>% 
-  group_by(Year,Landings,zone_id, Trip_ID)%>% 
-  filter(Landings=="Fish") %>%
-  summarize(weight.total=sum(weight.per.zone,na.rm = T))
-View(zone.fish.uni.trips.year)
+#zone.fish.uni.trips.year <- zones.fish%>% 
+#  group_by(Year,Landings,zone_id, Trip_ID)%>% 
+#  filter(Landings=="Fish") %>%
+#  summarize(weight.total=sum(weight.per.zone,na.rm = T))
+#View(zone.fish.uni.trips.year)
 
 #amount of fish caught per zone for each unique trip ID for each month
 zone.fish.uni.trips.monthly <- zones.fish%>% 
   group_by(Year,Month, Landings,zone_id, Trip_ID)%>% 
-  filter(Landings=="Fish") %>%
   summarize(weight.total=sum(weight.per.zone,na.rm = T))
 View(zone.fish.uni.trips.monthly)
 
 #joined tables showing amount of fish caught for total number of trips per zone per year
 joined.zone.fish.trips.year <- zone.fish.year %>% # join the log book and the fish data by unique Trip ID
-  filter(Landings =="Fish")%>%
   left_join(zone.fish.trips.year,by = c("Year", "Landings", "zone_id") )%>%
   mutate(avg.fish.per.trip=weight.total/Num.Trips)
 View(joined.zone.fish.trips.year)
 
 #joined tables showing amount of fish caught for trips per zone by gear type per year
 joined.zone.fish.trips.gear.year <- zone.fish.gear.year %>% # join the log book and the fish data by unique Trip ID
-  filter(Landings =="Fish")%>%
   left_join(zone.fish.trips.gear.year, by = c("Year", "Landings", "Gear", "zone_id") )%>%
   mutate(avg.fish.per.trip=weight.total/Num.Trips)
 View(joined.zone.fish.trips.gear.year)
 
 #joined tables showing amount of fish caught for total number of trips per zone per month
 joined.zone.fish.trips.monthly <- zone.fish.month %>% # join the log book and the fish data by unique Trip ID
-  filter(Landings =="Fish")%>%
   left_join(zone.fish.trips.monthly, by = c("Year", "Month", "Landings", "zone_id"))%>%
   mutate(avg.fish.per.trip=weight.total/Num.Trips)
 View(joined.zone.fish.trips.monthly)
 
 #joined tables showing amount of fish caught for trips per zone by gear type per month
 joined.zone.fish.trips.gear.monthly <- zone.fish.gear.month %>% # join the log book and the fish data by unique Trip ID
-  filter(Landings =="Fish")%>%
   left_join(zone.fish.trips.gear.monthly, by = c("Year", "Month", "Landings", "Gear", "zone_id") )%>%
   mutate(avg.fish.per.trip=weight.total/Num.Trips)
 View(joined.zone.fish.trips.gear.monthly)
@@ -342,21 +331,18 @@ zones.lob <- log.data %>%
  #amount of lobsters per zone per year
  zone.lob.year <- zones.lob %>% 
    group_by(Year,Landings,zone_id)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.lob.year)
  
  #amount of lobsters per zone per month
  zone.lob.month <- zones.lob %>% 
    group_by(Year,Month,Landings,zone_id)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.lob.month)
  
  #amount of lobsters per zone per year per gear
  zone.lob.gear.year<-zones.lob %>%
    group_by(Year, Landings, Gear, zone_id) %>%
-   filter(Landings=="Spiny Lobster") %>%
    select(Year, Landings, Gear, zone_id, ind.per.zone)%>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.lob.gear.year)  
@@ -364,7 +350,6 @@ zones.lob <- log.data %>%
  #amount of lobsters per zone per month per gear
  zone.lob.gear.month<-zones.lob %>%
    group_by(Year, Month, Landings, Gear, zone_id) %>%
-   filter(Landings=="Spiny Lobster") %>%
    select(Year, Month, Landings, Gear, zone_id, ind.per.zone)%>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.lob.gear.month)  
@@ -372,7 +357,6 @@ zones.lob <- log.data %>%
  #number of trips for lobsters per zone per year
  zone.lob.trips.year <- zones.lob %>% 
    group_by(Year,Landings,zone_id)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    select(Year, Landings, Trip_ID,zone_id) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.lob.trips.year)
@@ -380,7 +364,6 @@ zones.lob <- log.data %>%
  #number of trips for lobsters per zone per month
  zone.lob.trips.monthly <- zones.lob %>% 
    group_by(Year,Month,Landings,zone_id)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    select(Year, Month, Landings, Trip_ID,zone_id) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.lob.trips.monthly)
@@ -388,7 +371,6 @@ zones.lob <- log.data %>%
  #number of trips for lobsters per zone per year per gear
  zone.lob.trips.gear.year <- zones.lob %>% 
    group_by(Year,Landings,zone_id, Gear)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    select(Year, Landings, Trip_ID,zone_id, Gear) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.lob.trips.gear.year)
@@ -396,7 +378,6 @@ zones.lob <- log.data %>%
  #number of trips for lobsters per zone per month per gear
  zone.lob.trips.gear.monthly <- zones.lob %>% 
    group_by(Year,Month, Landings,zone_id, Gear)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    select(Year, Month, Landings, Trip_ID,zone_id, Gear) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.lob.trips.gear.monthly)
@@ -404,41 +385,35 @@ zones.lob <- log.data %>%
  #number of lobster caught per zone for each unique trip ID for each year
  zone.lob.uni.trips.year <- zones.lob%>% 
    group_by(Year,Landings,zone_id, Trip_ID)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.lob.uni.trips.year)
  
  #number of lobsters caught per zone for each unique trip ID for each month
  zone.lob.uni.trips.monthly <- zones.lob%>% 
    group_by(Year,Month, Landings,zone_id, Trip_ID)%>% 
-   filter(Landings=="Spiny Lobster") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.lob.uni.trips.monthly)
  
  #joined tables showing amount of lobsters caught for total number of trips per zone per year
  joined.zone.lob.trips.year <- zone.lob.year %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Spiny Lobster")%>%
    left_join(zone.lob.trips.year,  by = c("Year", "Landings", "zone_id") )%>%
    mutate(avg.lob.per.trip=ind.total/Num.Trips)
  View(joined.zone.lob.trips.year)
  
  #joined tables showing amount of lobsters caught for trips per zone by gear type per year
  joined.zone.lob.trips.gear.year <- zone.lob.gear.year %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Spiny Lobster")%>%
    left_join(zone.lob.trips.gear.year, by = c("Year", "Landings", "Gear", "zone_id") )%>%
    mutate(avg.lob.per.trip=ind.total/Num.Trips)
  View(joined.zone.lob.trips.gear.year)
  
  #joined tables showing amount of lobsters caught for total number of trips per zone per month
  joined.zone.lob.trips.monthly <- zone.lob.month %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Spiny Lobster")%>%
    left_join(zone.lob.trips.monthly, by = c("Year", "Month", "Landings", "zone_id"))%>%
    mutate(avg.lob.per.trip=ind.total/Num.Trips)
  View(joined.zone.lob.trips.monthly)
  
  #joined tables showing amount of lobsters caught for trips per zone by gear type per month
  joined.zone.lob.trips.gear.monthly <- zone.lob.gear.month %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Spiny Lobster")%>%
    left_join(zone.lob.trips.gear.monthly, by = c("Year", "Month", "Landings", "Gear", "zone_id") )%>%
    mutate(avg.lob.per.trip=ind.total/Num.Trips)
  View(joined.zone.lob.trips.gear.monthly)
@@ -462,21 +437,18 @@ zones.lob <- log.data %>%
  #amount of Queen Conch per zone per year
  zone.conch.year <- zones.conch %>% 
    group_by(Year,Landings,zone_id)%>% 
-   filter(Landings=="Queen Conch") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.conch.year)
  
  #amount of Queen Conch per zone per month
  zone.conch.month <- zones.conch %>% 
    group_by(Year,Month,Landings,zone_id)%>% 
-   filter(Landings=="Queen Conch") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.conch.month)
  
  #amount of Queen Conch per zone per year per gear
  zone.conch.gear.year<-zones.conch %>%
    group_by(Year, Landings, Gear, zone_id) %>%
-   filter(Landings=="Queen Conch") %>%
    select(Year, Landings, Gear, zone_id, ind.per.zone)%>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.conch.gear.year)  
@@ -484,7 +456,6 @@ zones.lob <- log.data %>%
  #amount of Queen Conch per zone per month per gear
  zone.conch.gear.month<-zones.conch %>%
    group_by(Year, Month, Landings, Gear, zone_id) %>%
-   filter(Landings=="Queen Conch") %>%
    select(Year, Month, Landings, Gear, zone_id, ind.per.zone)%>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.conch.gear.month)  
@@ -492,7 +463,6 @@ zones.lob <- log.data %>%
  #number of trips for Queen Conch per zone per year
  zone.conch.trips.year <- zones.conch %>% 
    group_by(Year,Landings,zone_id)%>% 
-   filter(Landings=="Queen Conch") %>%
    select(Year, Landings, Trip_ID,zone_id) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.conch.trips.year)
@@ -500,7 +470,6 @@ zones.lob <- log.data %>%
  #number of trips for Queen Conch per zone per month
  zone.conch.trips.monthly <- zones.conch %>% 
    group_by(Year,Month,Landings,zone_id)%>% 
-   filter(Landings=="Queen Conch") %>%
    select(Year, Month, Landings, Trip_ID,zone_id) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.conch.trips.monthly)
@@ -508,7 +477,6 @@ zones.lob <- log.data %>%
  #number of trips for Queen Conch per zone per year per gear
  zone.conch.trips.gear.year <- zones.conch %>% 
    group_by(Year,Landings,zone_id, Gear)%>% 
-   filter(Landings=="Queen Conch") %>%
    select(Year, Landings, Trip_ID,zone_id, Gear) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.conch.trips.gear.year)
@@ -516,7 +484,6 @@ zones.lob <- log.data %>%
  #number of trips for Queen Conch per zone per month per gear
  zone.conch.trips.gear.monthly <- zones.conch %>% 
    group_by(Year,Month, Landings,zone_id, Gear)%>% 
-   filter(Landings=="Queen Conch") %>%
    select(Year, Month, Landings, Trip_ID,zone_id, Gear) %>%
    summarize(Num.Trips=n_distinct(Trip_ID))
  View(zone.conch.trips.gear.monthly)
@@ -524,46 +491,40 @@ zones.lob <- log.data %>%
  #number of Queen Conch caught per zone for each unique trip ID for each year
  zone.conch.uni.trips.year <- zones.conch%>% 
    group_by(Year,Landings,zone_id, Trip_ID)%>% 
-   filter(Landings=="Queen Conch") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.conch.uni.trips.year)
  
  #number of Queen Conch caught per zone for each unique trip ID for each month
  zone.conch.uni.trips.monthly <- zones.conch%>% 
    group_by(Year,Month, Landings,zone_id, Trip_ID)%>% 
-   filter(Landings=="Queen Conch") %>%
    summarize(ind.total=sum(ind.per.zone,na.rm = T))
  View(zone.conch.uni.trips.monthly)
  
  #joined tables showing amount of Queen Conch caught for total number of trips per zone per year
  joined.zone.conch.trips.year <- zone.conch.year %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Queen Conch")%>%
    left_join(zone.conch.trips.year, by = c("Year", "Landings", "zone_id") )%>%
    mutate(avg.conch.per.trip=ind.total/Num.Trips)
  View(joined.zone.conch.trips.year)
  
  #joined tables showing amount of Queen Conch caught for trips per zone by gear type per year
  joined.zone.conch.trips.gear.year <- zone.conch.gear.year %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Queen Conch")%>%
    left_join(zone.conch.trips.gear.year, by = c("Year", "Landings", "Gear", "zone_id"))%>%
    mutate(avg.conch.per.trip=ind.total/Num.Trips)
  View(joined.zone.conch.trips.gear.year)
  
  #joined tables showing amount of Queen Conch caught for total number of trips per zone per month
  joined.zone.conch.trips.monthly <- zone.conch.month %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Queen Conch")%>%
    left_join(zone.conch.trips.monthly, by = c("Year", "Month", "Landings", "zone_id"))%>%
    mutate(avg.conch.per.trip=ind.total/Num.Trips)
  View(joined.zone.conch.trips.monthly)
  
  #joined tables showing amount of Queen Conch caught for trips per zone by gear type per month
  joined.zone.conch.trips.gear.monthly <- zone.conch.gear.month %>% # join the log book and the fish data by unique Trip ID
-   filter(Landings =="Queen Conch")%>%
    left_join(zone.conch.trips.gear.monthly, by = c("Year", "Month", "Landings", "Gear", "zone_id") )%>%
    mutate(avg.conch.per.trip=ind.total/Num.Trips)
  View(joined.zone.conch.trips.gear.monthly)
  
- ####################################### Zone Fish Data by Species #####################
+ ####################################### Zone Fish Data by Species #########################
  GCRM.data.Fish <- import(paste0(input.dir,"GCRMN FISH BIOMASS DATA EUX 2018.xlsx"),
                         which = 6, skip =0, .name_repair="universal")   #import sheet for fish LW values
  names(GCRM.data.Fish)
@@ -612,6 +573,49 @@ zones.lob <- log.data %>%
  summarize(fish.weight.total=sum(ind.fish.weight))
  #count(Species_latin_name, name = "Num.ind")
  View(zone.species.weight.year)
+ 
+ ################################# adding zone areas and initial map making #####################
+ input.dir <- "/Users/gcullinan//OneDrive - Duke University/MP Project/spatial-fisheries-analysis/Data/"
+ 
+ allfiles <- list.files(input.dir,recursive = T, full.names = T) 
+ 
+ # Select kml files with 1) digit then 1 letter, 2) digit then 2 letters, 3) digit then .kml, 4) digit then buffer
+ file.list <- c(grep("Zone_[0-9]{1}.kml",allfiles,value = T),
+                grep("FAD_[0-9]_[a-z]*",allfiles,value = T))
+ 
+ zone.ind <- st_read(file.list[1])
+ zone.ind$zone_id <- as.character(gsub("Zone_","", zone.ind$Name)) # removes all non-digit characters
+ #zone.ind$zone_id <- as.character(gsub(".kml","",zone.ind$Name)) # removes all non-digit characters
+ 
+ for (i in (2:length(file.list))) {
+   # retrieve kml 
+   X <- st_read(file.list[i]) 
+   # extract zone_id from file name 
+   X$zone_id <- as.character(gsub("Zone_","", X$Name)) # removes all non-digit characters
+   #X$zone_id <- as.character(gsub(".kml","",X$zone_id)) # removes all non-digit characters
+   # combine X to the previous shp
+   zone.ind <- rbind(zone.ind,X) 
+ }
+ zone.ind$zone_id <- gsub("/Users/gcullinan//OneDrive - Duke University/MP Project/spatial-fisheries-analysis/Data/","",zone.ind$zone_id)
+ 
+ # view map
+ plot(st_geometry(zone.ind))
+ 
+ # get area
+ zone.ind$area <- st_area(zone.ind)
+ zone.area <-zone.ind%>%
+   rename(area_m2 = area)
+ zone.area$area_m2<-gsub(" [m^2]", "", zone.area$area_m2)
+ 
+ # Example join
+ fishing.zones <-zone.fish.year%>%
+   mutate (zone_id = as.character(zone_id))
+ 
+ zone.ind2 <- zone.area %>% 
+   left_join(fishing.zones, by = "zone_id")%>%
+   mutate(area_m2 = as.numeric(area_m2))%>%
+   mutate(area_km=area_m2/1000000)
+ 
  
  
 #log.data <- ifelse(log.data$Year==2004,2014,log.data$Year)
