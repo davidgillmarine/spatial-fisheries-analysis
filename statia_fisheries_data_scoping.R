b17bc00c6a354e118ea7fdfb8b6d76a422892595
danagrieco <danagrieco@users.noreply.github.com>
2023-02-07 13:02:18 -0500

    Code finished being cleaned, this entire file works now

--- statia_fisheries_data_scoping.R
+++ statia_fisheries_data_scoping.R
@@ -11,21 +11,20 @@ source("fisheries_summary_functions.R")
 
 fish.weight.year<-my_landings(log.data,"all","Fish",weight.kg,Year) #amount of fish caught per year for the whole fishing area
 head(fish.weight.year) #this is used to check and make sure that the data was analyzed correctly
-export(fish.weight.year,"Final_Figures_Tables/yearly_fishing_effort.xlsx") # This is to export the number of landings and intensity of fishing 
+export(fish.weight.year,"Final_Figures_Tables-yearly_fishing_effort.xlsx") # This is to export the number of landings and intensity of fishing 
   # for each year for table in paper 
-
 
 fish.weight.inpark.year<-my_landings(log.data,"park","Fish",weight.kg,Year) #amount of fish caught per year inside the park ##double check with archive 
 head(fish.weight.inpark.year) #check your work
-export(fish.weight.inpark.year, "Final_Figures_Tables/yearly_fishing_effort_inpark.xlsx") 
+export(fish.weight.inpark.year, "Final_Figures_Tables-yearly_fishing_effort_inpark.xlsx") 
 
-#looking at the amount of fish caught per month, looking at sesonality 
+#looking at the amount of fish caught per month, looking at seasonality 
 fish.weight.month<-my_landings(log.data,"all","Fish",weight.kg,Year,Month)
 #looking at the amount of fish caught by gear type
 fish.gear<-my_landings(log.data,"all","Fish",weight.kg,Gear)
 #looking at the amount of fish caught by year and gear type
 fish.gear.year<-my_landings(log.data,"all","Fish",weight.kg,Year,Gear)
-#looking at the amount of fish caught by month, and gear type loooking for seasonal gear usage
+#looking at the amount of fish caught by month, and gear type looking for seasonal gear usage
 fish.gear.month<-my_landings(log.data,"all","Fish",weight.kg,Month,Gear)
 
 
@@ -48,8 +47,5 @@ head(zone.fish.inpark.year)
 #looking at the amount of fish caught within the park, aka from 0-30m in depth, for each year, by gear and zone
 zone.fish.inpark.gear.year<-my_landings(log.data.zone,"park","Fish",weight.per.zone,Year,Gear,zone_id)
 head(zone.fish.inpark.gear.year)
-
- 
-
 
 
