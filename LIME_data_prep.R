cca7904c955517db86b1a3b855002b0951a58eb4
danagrieco <danagrieco@users.noreply.github.com>
2023-05-24 11:41:47 -0400

    switching common name to latin name for higher accuracy

--- LIME_data_prep.R
+++ LIME_data_prep.R
@@ -11,31 +11,32 @@ input.dir <- 'R:/Gill/research/spatial-fisheries-analysis/tables/raw/' #set the 
 fish.dat <- import(paste0(input.dir,"Statia logbook Raw data last update October 2022.xlsx"), #import the correct file and the page of the fisheries 
                    which = "Fish")  %>%     #spreadsheet and tell it where to start from the top
   clean_names() %>% 
-  filter(!is.na(species_common_name))
+  filter(!is.na(species_latin_name))
 
 
 #check names and fish
 names(fish.dat)
-unique(fish.dat$species_common_name)
+unique(fish.dat$species_latin_name)
 
 # create fake data to get seq 0:50 columns
-fake.dat <- data.frame(species_common_name="",length_cm=seq(0,50,1)) # create fake data
+fake.dat <- data.frame(species_latin_name="",length_cm=seq(0,50,1)) # create fake data
 fake.dat <- fish.dat %>% 
   slice(0) %>%  # get column names from fish.dat
   bind_rows(fake.dat) # add fake data
 
 
-
 fish.dat.gp <- fish.dat %>% 
   mutate(length_cm=as.integer(round(length_cm))) %>%  # round up
   bind_rows(fake.dat) %>% # add fake data
-  group_by(species_common_name,year,length_cm) %>% 
+  group_by(species_latin_name,year,length_cm) %>% 
   summarise(num=n()) # count unique values
 head(fish.dat.gp)
 
 fish.dat.gp1 <- fish.dat.gp %>% 
   spread(length_cm,num,fill = 0) %>% # spread to columns
-  filter(!is.na(species_common_name) & !(species_common_name=="")) # get rid of blank values
+  filter(!is.na(species_latin_name) & !(species_latin_name=="")) # get rid of blank values
 
 
 fish.dat.gp1 %>% view()
+  #NOTE: there is one species who does not have it's species latin name listed - need to try and find it via common name
+
